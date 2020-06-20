db692365b860a62a6363b4cabe339b24b84c5fc1
Devsome <info@devsome.com>
2020-06-20 02:13:04 +0200

    Frontend - E-Mail vendor publish & added Logo

---
+++ resources/views/vendor/mail/html/message.blade.php
@@ -0,0 +1,28 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{ config('app.name') }}
+            <img src="{{ asset('image/logo/logo.png') }}" height="40" alt="{{ config('app.name') }}"> {{ config('app.name') }}
+        @endcomponent
+    @endslot
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+        @endcomponent
+    @endslot
+@endcomponent
