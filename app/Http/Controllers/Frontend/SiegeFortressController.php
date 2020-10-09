1a574b0043689ef3e666d744bee7326854be77cf
Devsome <info@devsome.com>
2020-10-10 00:50:54 +0200

    Fortress fixed

--- app/Http/Controllers/Frontend/SiegeFortressController.php
+++ app/Http/Controllers/Frontend/SiegeFortressController.php
@@ -14,8 +14,8 @@ class SiegeFortressController extends Controller
         $fortressMapping = [
             1 => __('sidebar.fortress.jangan'),
             3 => __('sidebar.fortress.hotan'),
+            4 => __('sidebar.fortress.constantinople'),
             6 => __('sidebar.fortress.bandit'),
-            9 => __('sidebar.fortress.constantinople'),
         ];
         $siteSettings = config('siteSettings');
 
@@ -42,11 +42,11 @@ class SiegeFortressController extends Controller
                         if ($data->FortressID === 3) {
                             $data->FortressImage = asset('image/sro/etc/fort_hotan.jpg');
                         }
-                        if ($data->FortressID === 6) {
-                            $data->FortressImage = asset('image/sro/etc/fort_bijeokdan.jpg');
-                        }
                         if ($data->FortressID === 4) {
                             $data->FortressImage = asset('image/sro/etc/fort_constantinople.jpg');
+                        }
+                        if ($data->FortressID === 6) {
+                            $data->FortressImage = asset('image/sro/etc/fort_bijeokdan.jpg');
                         }
                     }
                     return $data;
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>{{ config('app.name', 'Admin') }}</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('image/sdl.png') }}">
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
           rel="stylesheet">
