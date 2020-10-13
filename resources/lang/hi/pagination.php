b337aaf10518ac7287cbfdaca2fb133277b255fa
Saifur Rahman Mohsin <mohsin92@me.com>
2020-10-13 17:34:26 +0530

    Hindi Translation
    Add the hindi translation for Silkroad Online.

---
+++ resources/lang/hi/pagination.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'previous' => '&laquo; पिछला',
+    'next' => 'आगे &raquo;',
+];
---
+++ resources/lang/hi/seo.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'index' => 'पहला पेज - ' . config('app.name'),
+    'home' => 'डैशबोर्ड - ' . config('app.name'),
+    'charlist' => 'व्यक्ति सूची - ' . config('app.name'),
+    'donations' => 'डोनेशन - ' . config('app.name'),
+    'settings' => 'समायोजन - ' . config('app.name'),
+    'referral' => 'रेफरल - ' . config('app.name'),
+    'voucher' => 'वाउचर - ' . config('app.name'),
+    'tickets' => [
+        'index' => 'टिकट - ' . config('app.name'),
+        'new' => 'नया टिकट बनाएं - ' .config('app.name'),
+        'show' => ':name - ' . config('app.name')
+    ],
+    'webinventory' => 'वेब इन्वेंटरी - ' . config('app.name'),
+    'downloads' => 'डाउनलोड - ' . config('app.name'),
+    'notification' => 'नोटिफिकेशन - ' . config('app.name'),
+    'rules' => 'नियम - ' . config('app.name'),
+    'worldmap' => 'दुनिया का नक्शा - ' . config('app.name'),
+    'news' => [
+        'index' => ':name - ' . config('app.name'),
+        'archive' => 'समाचार का संग्रह - ' . config('app.name')
+    ],
+    'ranking' => [
+        'index' => ':name की रैंकिंग - ' . config('app.name'),
+    ],
+    'auctionshouse' => [
+        'index' => 'नीलामी घर - ' . config('app.name'),
+        'own' => 'स्वयं के नीलामी आइटम दिखाएं - ' . config('app.name'),
+        'add' => 'नीलामी आइटम जोड़ें - ' . config('app.name'),
+        'showitem' => ':name की नीलामी हाउस - ' . config('app.name'),
+    ],
+    'information' => [
+        'player' => 'व्यक्ति :name - ' . config('app.name'),
+        'guild' => 'समाज :name - ' . config('app.name')
+    ],
+    'serverinformation' => 'सर्वर के बारे में जानकारी - ' . config('app.name'),
+];
