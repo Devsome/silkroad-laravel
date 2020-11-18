b0a5c358491a73a76b27d6f97c2413c9e0d12530
Belal El Menshawy <40501032+deadly009@users.noreply.github.com>
2020-11-19 00:28:18 +0200

    Update 2020_10_31_022911_create_webcms_procedure.php

--- database/migrations/2020_10_31_022911_create_webcms_procedure.php
+++ database/migrations/2020_10_31_022911_create_webcms_procedure.php
@@ -95,7 +95,7 @@ BEGIN
                                            INNER JOIN [{$databaseShard}].[dbo]._Inventory IV ON IV.CharID = CH.CharID
                                            INNER JOIN [{$databaseShard}].[dbo]._Items IT ON IT.ID64 = IV.ItemID
                                            INNER JOIN [{$databaseShard}].[dbo]._RefObjCommon RO ON IT.RefItemID = RO.ID
-[]                                  WHERE IV.slot < 13
+                                    WHERE IV.slot < 13
                                     AND IV.slot <> 8
                                     AND IV.slot <> 7
                                     AND IV.CharID = CH.CharID
