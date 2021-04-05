ec8133a42f7a9b6c9a4760b054ca0797d9ac9d85
Ahmed Elrayes <ahmedwaill63@gmail.com>
2021-04-05 18:42:55 +0200

    file was deleted by mistake.

---
+++ config/language.example.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Used for the application language
+ */
+return [
+    'tr' => [
+        'name' => 'Türkçe',
+        'icon' => '/image/flags/Turkey.png'
+    ],
+    'en' => [
+        'name' => 'English',
+        'icon' => '/image/flags/United_States_of_America.png'
+    ],
+    'es' => [
+        'name' => 'Español',
+        'icon' => '/image/flags/Spain.png'
+    ],
+    'vn' => [
+        'name' => 'Vietnam',
+        'icon' => '/image/flags/Vietnam.png'
+    ],
+    'hi' => [
+        'name' => 'हिन्दी',
+        'icon' => '/image/flags/India.png'
+    ],
+    'pt' => [
+        'name' => 'Portugues',
+        'icon' => '/image/flags/Portugal.png'
+    ],
+    'ar' => [
+        'name' => 'العربية',
+        'icon' => '/image/flags/Saudi_Arabia.png'
+    ]
+];
